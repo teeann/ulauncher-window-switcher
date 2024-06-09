26c879bd6e7e7148dfc32430aa1edd12bbec413b
Anh T Nguyen <nguyentheanh2903@gmail.com>
2024-06-09 11:24:26 +0700

    fix bug, only display window app name

--- main.py
+++ main.py
@@ -40,47 +40,28 @@ class KeywordQueryEventListener(EventListener):
         # Get list of all windows, and process into a dictionary that looks like this:
         # {<window_id>: {ws: <workspace_id>, name: <window_name>}}
         result = subprocess.run(
-            ['wmctrl -l | awk \'{ if( $2 != "-1") { $3="";  print $0} }\''],
+            ['wmctrl -xl | awk \'{ if( $2 != "-1") {print $1","$3} }\''],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,  # equivalent to capture_output=True
             shell=True,
             universal_newlines=True,  # equivalent to text=True
-        ).stdout
-        w_list = [y for y in (x.strip() for x in result.split("\n")) if y]
-        w_dict = {
-            x[1].split(maxsplit=2)[0]: {
-                "ws": int(x[1].split(maxsplit=2)[1]),
-                "name": x[1].split(maxsplit=2)[2],
-            }
-            for x in enumerate(w_list)
-        }
+        ).stdout.split("\n")
 
-        # Get list of all workspaces and process into a dictionary that looks like this:
-        # {<workspace_id>: <workspace_name>}
-        result = subprocess.run(
-            [
-                "wmctrl -d | sed -n -E -e 's/^.*WA: (N\/A|.,. [[:digit:]]+x[[:digit:]]+)  //p'"
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,  # equivalent to capture_output=True
-            shell=True,
-            universal_newlines=True,  # equivalent to text=True
-        ).stdout
-        ws_list = [y for y in (x.strip() for x in result.splitlines()) if y]
-        ws_dict = {i: x for i, x in enumerate(ws_list)}
+        w_list = []
+        for w in result:
+            if w == '':
+                continue
+            w = w.strip().split(",")
+            wid = w[0]
+            wdisplay = w[1].split('.')[-1]
+            w_list.append([wid, wdisplay])
 
-        for w_idx, window in w_dict.items():
-            if search == "" or search in window["name"].lower():
+        for w_idx, window in w_list:
+            if search == "" or search in window.lower():
                 items.append(
                     ExtensionResultItem(
                         icon="images/window.svg",
-                        # Workaround for https://github.com/Ulauncher/Ulauncher/issues/587
-                        name=window["name"].replace("&", "&amp;")
-                        if search
-                        else window["name"],
-                        description="Workspace {}: {}, Window Id: {}".format(
-                            window["ws"], ws_dict[window["ws"]], w_idx
-                        ),
+                        name=window,
                         on_enter=RunScriptAction("wmctrl -ia {}".format(w_idx)),
                     )
                 )
